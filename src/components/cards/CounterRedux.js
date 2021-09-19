cc2c4cec44ce9aa2a538909cd9f803550a6066f1
Iván <ivanmeyer1991@gmail.com>
2021-09-19 20:38:00 -0300

    Style Amount modified:   src/components/cards/CounterRedux.js

--- src/components/cards/CounterRedux.js
+++ src/components/cards/CounterRedux.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { deposit, withdraw } from "../../store/amount/action";
 import { Button } from "@chakra-ui/button";
-import {selectCurrentAmount} from "../../store/amount/reducer"
+import { selectCurrentAmount } from "../../store/amount/reducer";
 import { connect } from "react-redux";
+import { Text } from "@chakra-ui/layout";
 
 const mapStateToProps = (state) => {
   return {
@@ -13,7 +14,7 @@ const mapStateToProps = (state) => {
 const CounterRedux = ({ amount, deposit, withdraw }) => {
   return (
     <div>
-      <h1>{amount}</h1>
+      <Text color="#FFFFFF">{amount}</Text>
       <Button onClick={() => deposit()}>Deposit $10</Button>
       <Button onClick={() => withdraw()}>Withdraw $10</Button>
     </div>
